8180289532ce96960d9bf83cc30941555a68b82b
N Aditya Sai <69337330+aadityasai37@users.noreply.github.com>
2022-04-26 01:37:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spider import Spider
 from domain import *
 from general import *
 
-PROJECT_NAME = 'navy'
-HOMEPAGE = 'https://indiannavy.nic.in	'
+PROJECT_NAME = 'SRM Wesbite'
+HOMEPAGE = 'https://srmist.edu.in'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
